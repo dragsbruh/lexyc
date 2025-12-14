726fc023d0cb8469793b9db3efc0403a6ea876f2
dragsbruh <dragsbruh@protonmail.com>
2025-12-14 16:53:58 +0530

    chore: add original xycc result to test

--- test.sh
+++ test.sh
@@ -2,17 +2,42 @@
 
 set -eu
 
+mkdir -p _build/
+
 zig build
 
-mkdir -p _build/
+if [ ! -f _build/xy/xycc ];then
+  repodir="_build/.repo.xy.tmp"
+  cwdir=$(pwd)
+
+  rm -rf _build/xy $repodir
+
+  git clone https://git.disroot.org/nora.aoki/xy "$repodir"
+  cd $repodir
+  meson build
+  cd build
+  ninja
+
+  cd "$cwdir"
+
+  mkdir -p _build/xy/
+  mv $repodir/build/xycc _build/xy/xycc.tmp
+  rm -rf $repodir
+
+  mv _build/xy/xycc.tmp _build/xy/xycc
+fi;
+
+./_build/xy/xycc examples/helloworld.xy _build/xy/out.ir
+lli ./_build/xy/out.ir > _build/xy.txt
+
 ./zig-out/bin/lexyc examples/helloworld.xy debug _build/debug.txt
 ./zig-out/bin/lexyc examples/helloworld.xy interpreter _build/interpreter.txt
 
-mkdir -p _build/nasm-linux_x86_64
-./zig-out/bin/lexyc examples/helloworld.xy nasm-linux_x86_64 _build/nasm-linux_x86_64/asm.s
-nasm -felf64 _build/nasm-linux_x86_64/asm.s -o _build/nasm-linux_x86_64/obj.o
-ld _build/nasm-linux_x86_64/obj.o -o _build/nasm-linux_x86_64/exe
-./_build/nasm-linux_x86_64/exe > _build/nasm-linux_x86_64.txt
+# mkdir -p _build/nasm-linux_x86_64
+# ./zig-out/bin/lexyc examples/helloworld.xy nasm-linux_x86_64 _build/nasm-linux_x86_64/asm.s
+# nasm -felf64 _build/nasm-linux_x86_64/asm.s -o _build/nasm-linux_x86_64/obj.o
+# ld _build/nasm-linux_x86_64/obj.o -o _build/nasm-linux_x86_64/exe
+# ./_build/nasm-linux_x86_64/exe > _build/nasm-linux_x86_64.txt
 
 mkdir -p _build/nasm-linux_x86_32
 ./zig-out/bin/lexyc examples/helloworld.xy nasm-linux_x86_32 _build/nasm-linux_x86_32/asm.s
